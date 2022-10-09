4f66f934979833fbc339fede5bda7422c93d34b1
Takako Onozuka <onozukatakako@gmail.com>
2022-10-09 17:08:31 +0100

    fixed ordering of list elements

--- models/record.py
+++ models/record.py
@@ -1,11 +1,11 @@
 class Record:
-    def __init__(self, title, artist, genre, year, buying_cost, price, stock_count, id = None):
+    def __init__(self, title, artist, genre, year, stock_count, buying_cost, price, id = None):
         self.title = title
         self.artist = artist
         self.genre = genre
         self.year = year
+        self.stock_count = stock_count
         self.buying_cost = buying_cost
         self.price = price
-        self.stock_count = stock_count
         self.id = id
        
--- repositories/record_repository.py
+++ repositories/record_repository.py
@@ -5,9 +5,10 @@ from models.artist import Artist
 import repositories.artist_repository as artist_repository
 
 def save(record):
-    sql = "INSERT INTO records (title, artist_id, genre, year, buying_cost, price, stock_count) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
-    values = [record.title, record.artist.id, record.genre, record.year, record.buying_cost, record.price, record.stock_count]
+    sql = "INSERT INTO records (title, artist_id, genre, year, stock_count, buying_cost, price) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [record.title, record.artist.id, record.genre, record.year, record.stock_count, record.buying_cost, record.price]
     results = run_sql(sql, values)
+    breakpoint()
     id = results[0]['id']
     record.id = id
     return record
@@ -20,7 +21,7 @@ def select_all():
 
     for row in results:
         artist = artist_repository.select(row['artist_id'])
-        record = Record(row['title'], artist, row['genre'], row['year'], row['buying_cost'], row['price'], row['stock_count'], row['id'])
+        record = Record(row['title'], artist, row['genre'], row['year'], row['stock_count'], row['buying_cost'], row['price'], row['id'])
         records.append(record)
     return records
 
@@ -33,7 +34,7 @@ def select(id):
     if results:
         result = results[0]
         artist = artist_repository.select(result['artist_id'])
-        record = Record(result['title'], artist, result['genre'], result['year'], result['buying_cost'], result['price'], result['stock_count'], result['id'] )
+        record = Record(result['title'], artist, result['genre'], result['year'],  result['stock_count'], result['buying_cost'], result['price'], result['id'] )
     return record
 
 def delete_all():
@@ -46,7 +47,7 @@ def delete(id):
     run_sql(sql, values)
 
 def update(record):
-    sql = "UPDATE records SET (title, artist_id, genre, year, buying_cost, price, stock_count, id) = (%s, %s, %s, %s, %s, %s, %s, %s) WHERE id = %s"
-    values = [record.title, record.artist.id, record.genre, record.year, record.bying_cost, record.price, record.stock_count, record.id]
+    sql = "UPDATE records SET (title, artist_id, genre, year, stock_count, buying_cost, price, id) = (%s, %s, %s, %s, %s, %s, %s, %s) WHERE id = %s"
+    values = [record.title, record.artist.id, record.genre, record.year, record.stock_count,  record.buying_cost, record.price, record.id]
     run_sql(sql, values)
 
